1b9e1cabe3c9ee80d1ae2d796da01bfaf815152d
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 01:38:52 -0400

    config production

--- app/settings/base.py
+++ app/settings/base.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
 }
 
