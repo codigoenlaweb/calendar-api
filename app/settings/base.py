1169667886b2e9aef33c5966eb0300f0fa795e20
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 01:34:08 -0400

    config production

--- app/settings/base.py
+++ app/settings/base.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip --default-timeout=100 install types-cryptography
-poetry add
+poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
