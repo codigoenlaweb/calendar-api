9c465e715066279956f18fe760ab928dd155abcb
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 00:54:34 -0400

    config build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+pip --default-timeout=100 install types-cryptography
 poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
