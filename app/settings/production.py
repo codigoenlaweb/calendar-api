b6b8d51c875d3f05f566340404a26f153d3b2f0b
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 08:21:31 -0400

    production

--- app/settings/production.py
+++ app/settings/production.py
@@ -1,43 +1,34 @@
-import dj_database_url
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=['*']))
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default=env.db_url('DATABASE_URL', default='postgresql://postgres:postgres@localhost:5432/mysite'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('NAME_DB'),
+        'USER': env.str('USER_DB'),
+        'PASSWORD': env.str('PASSWORD_DB'),
+        'HOST': env.str('HOST_DB'),
+        'PORT': env.int('PORT_DB')
+    }
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR.parent, 'static'),)
-
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # email
 EMAIL_BACKEND = env.str('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = env.str('EMAIL_HOST', default='your.email.host')
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
-EMAIL_PORT = env.int('EMAIL_PORT', default=587)
+EMAIL_HOST = env.str('EMAIL_HOST', default='your.email.host')
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default='your email host user')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default='your email host password')
+EMAIL_PORT = env.int('EMAIL_PORT', default=2525)
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-pip --default-timeout=100 install types-cryptography
-poetry install
-python manage.py collectstatic --no-input
-python manage.py migrate
