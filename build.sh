d757cdf0491c6d6f3e55ed448cce637f8109a953
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 00:24:13 -0400

    config build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit
-poetry install
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
