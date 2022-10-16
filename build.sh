9aadc9b0999214891246eb8c241dd771beca6d16
codigoenlaweb <codeenlaweb@gmail.com>
2022-10-16 01:28:31 -0400

    config production

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip --default-timeout=100 install types-cryptography
-poetry install
+poetry add
 python manage.py collectstatic --no-input
 python manage.py migrate
